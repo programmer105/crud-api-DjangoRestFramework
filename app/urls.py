50f1a2caa3f532d6a9745ac4c6b7d37b95de5059
Lucas Daniel <lucas.dmdf@bol.com.br>
2022-11-23 15:38:21 -0300

    updating api using viewsets

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
+from app.views import TodoViewSet
 
-urlpatterns = [
-    path('',TodoListAndCreate.as_view()),
-    path('<int:pk>/', TodoDetailChangeAndDelete.as_view() ),
-]
+from rest_framework.routers import DefaultRouter
 
-
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+urlpatterns = router.urls
--- app/views.py
+++ app/views.py
@@ -2,21 +2,13 @@ from app.models import Todo
 from app.serializers import TodoSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
-from rest_framework import generics
 
 
-
-
-class TodoListAndCreate(generics.ListCreateAPIView):
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
- 
-        
-class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
- 
-    
+
+
